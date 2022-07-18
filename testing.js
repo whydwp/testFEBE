16a3974b5a77b6f504e1949f7dad3a7f98ce9dc6
wahyuwww <wahyu.aan2508@gmail.com>
2022-07-18 19:13:08 +0700

    fisrt upload

---
+++ testing.js
@@ -0,0 +1,34 @@
+
+
+var number = 0;
+while (number < 50) {
+  number++;
+  let message = "";
+  if (number % 3 === 0) message += "Frontend";
+  if (number % 5 === 0) message += "Backend";
+  console.log(message || number);
+
+  const splitByNumber = {
+    [Symbol.split](str) {
+      let num = 1;
+      let pos = 0;
+      const result = [];
+      while (pos < str.length) {
+        const matchPos = str.indexOf(num, pos);
+        if (matchPos === -1) {
+          result.push(str.substring(pos));
+          break;
+        }
+        result.push(str.substring(pos, matchPos));
+        pos = matchPos + String(num).length;
+        num++;
+      }
+      return result;
+    },
+  };
+
+  const myString = message || number;
+  console.log(myString.split(splitByNumber));
+}
+
+
